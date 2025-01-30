bf8bf5c6b89e5ae97b5f70b67bf50b978f4fd6c5
waffle <nate.crema@gmail.com>
2025-01-30 14:03:12 +0900

    add functioons

---
+++ action.js
@@ -0,0 +1,66 @@
+const _ = require('lodash')
+const Jira = require('./common/net/Jira')
+
+module.exports = class {
+  constructor ({ githubEvent, argv, config }) {
+    this.Jira = new Jira({
+      baseUrl: config.baseUrl,
+      token: config.token,
+      email: config.email,
+    })
+
+    this.config = config
+    this.argv = argv
+    this.githubEvent = githubEvent
+  }
+
+  async execute () {
+    const { argv } = this
+
+    const projectKey = argv.project
+    const issueId = argv.issue
+
+    const issue = await this.Jira.getIssue(issueId)
+    if (!issue) {
+      console.log(`Issue Not Found`);
+      return
+    }
+
+    let providedFields = [{
+      key: 'project',
+      value: {
+        key: projectKey,
+      },
+    }, {
+      key: 'summary',
+      value: argv.summary,
+    }]
+
+    if (argv.description) {
+      providedFields.push({
+        key: 'description',
+        value: argv.description,
+      })
+    }
+
+    if (argv.fields) {
+      providedFields = [...providedFields, ...this.transformFields(argv.fields)]
+    }
+
+    const payload = providedFields.reduce((acc, field) => {
+      acc.fields[field.key] = field.value
+
+      return acc
+    }, {
+      fields: {},
+    })
+
+    await this.Jira.updateIssue(issueId, payload);
+
+    // console.log(`transitionedIssue:${JSON.stringify(transitionedIssue, null, 4)}`)
+    console.log(`Update ${issueId} complete.`)
+    console.log(`Link to issue: ${this.config.baseUrl}/browse/${issueId}`)
+
+    return {}
+  }
+}
