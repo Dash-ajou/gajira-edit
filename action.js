5f209d4449e2f54321b96d73098a0d223f37ba86
waffle <nate.crema@gmail.com>
2025-02-09 23:36:39 +0900

    debug: test 2

--- action.js
+++ action.js
@@ -45,7 +45,7 @@ module.exports = class {
     let providedFields = []
 
     if (argv.summary) {
-      console.log("Setting summary: ", argv.summary.replaceAll(`\[${projectKey}-${issueId}\] `, ""));
+      console.log("Setting summary: ", projectKey, issueId, argv.summary.replaceAll(`\[${projectKey}-${issueId}\] `, ""));
       providedFields.push({
         key: 'summary',
         value: argv.summary.replaceAll(`\[${projectKey}-${issueId}\] `, ""),
